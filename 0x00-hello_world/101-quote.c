c67412ade2d97e65af56a5a8fc9fbf5c5990257a
Shamso Osman <osmanshamso004@gmail.com>
2022-03-10 10:59:44 -0800

    quite

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <unistd.h>
 /**
-*main - Enty point
+*main -Entry point
 *
-*Return: Always 1 (success)
+*Return: Always 1 (Success)
 */
 int main(void)
 {
-	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19
+[D\n";
 
 	write(2, str1, 59);
-	return(1);
+	return (1);
 }
