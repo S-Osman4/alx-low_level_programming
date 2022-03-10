cf541ef1626abe20efdb4173e995e1b339e709e4
Shamso Osman <osmanshamso004@gmail.com>
2022-03-10 10:32:12 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 /**
-*main - Entry point
+*main-Entry point
 *
-*Return : Alwys0 (success)
+*Return: Always 0 (success)
 */
 int main(void)
 {
-	char srt[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, str1, 59)
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
+	write(2, str1, 59);
 	return (1);
 }
