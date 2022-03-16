e0b5da81624a3cc8747c67a77927e0f16db70ba3
Shamso Osman <osmanshamso004@gmail.com>
2022-03-16 05:40:53 -0700

    update file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-* main - tests function that prints if integer is positive or negative
+* main - tests function that prints if int is +ve or -ve
 * Return: 0
 */
 int main(void)
 {
 	int i;
 
-	i = 98;
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
