e3c437fc69bcb75cc9a27538c27ccadf40af286d
Shamso Osman <osmanshamso004@gmail.com>
2022-03-22 07:55:11 -0700

    add changes

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,24 +1,27 @@
 #include "main.h"
 /**
-* _atoi - Converts a string to an integer.
-* @s: The string to be converted.
-* Return: The integer value of the converted string.
+* _atoi - convert a string to an integer.
+* @s: the string to be comverted
+*Return: The integer value of the comverted string.
 */
+
 int _atoi(char *s)
 {
 	int sign = 1;
 	unsigned int num = 0;
 
 	do {
-#include "main.h"
-/**
-* _atoi - Convert a string to integer.
-* @s: char array string
-* Description: Number in the string can be preceded by an infinite
-* number of characters.
-* Return: first integer found in string
-*/
-int _atoi(char *s)
-{
-	return 0;
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+	}
+	while (*s++);
+
+	return (num * sign);
 }
