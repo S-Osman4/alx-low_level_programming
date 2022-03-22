0cb7013d7b156c45ee624e0ce665fad611c0f5a7
Shamso Osman <osmanshamso004@gmail.com>
2022-03-22 08:02:24 -0700

    add chnges

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>#include <time.h>
+
 /**
-* main - generate random valid passwords over 101-crackme
-* Return: 
-* Return: 0
+*
+*
 */
 int main(void)
 {
-	int rndm, counter, checksum;
+	int sum;
+	char c;
 
 	srand(time(NULL));
-
-	for (counter = 0, checksum = 2772; checksum > 122; counter++)
+	while (sum <= 2645)
 	{
-		rndm = (rand() % 100);
-		printf("%c", rndm);
-		checksum -= rndm;
+		c = rand() % 128;
+		sum += c;
+
+		putchar(c);
 	}
-	printf("%c", checksum);
+	putchar(2772 - sum);
 	return (0);
 }
