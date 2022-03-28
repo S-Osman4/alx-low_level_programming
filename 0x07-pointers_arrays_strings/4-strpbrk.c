9222da69dbe3c37f5c765fcc0868315b2410d141
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-03-28 16:24:22 +0300

    gets length of prefix substring

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,1 +1,24 @@
 #include "main.h"
+/**
+* _strspn - gets length of prefix substring
+* @s: string to check
+* @accept: bytes composing prefix
+* Return: integer length of substring
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0, j, b = 0;
+
+	for (; s[i] && s[i] != ' '; i++)
+	{
+		j = 0;
+
+		for (; accept[j]; j++)
+		{
+			if (accept[j] == s[i])
+				b++;
+		}
+	}
+
+	return (b);
+}
