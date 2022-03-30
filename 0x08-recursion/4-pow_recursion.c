1d92c10fd5fab918d4dcfc1c855d2434208e1e40
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-03-30 14:39:36 +0300

    returns the value of x to the power of y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _pow_recursion - Function that returns the value of x to the power of y.
- * @x: type int
- * @y: type int
- * Return: y lower than 0 function -1.
- */
+* _pow_recursion - Function that returns the value of x to the power of y.
+* @x: type int
+* @y: type int
+* Return: y lower than 0 function -1.
+*/
 
 int _pow_recursion(int x, int y)
 {
