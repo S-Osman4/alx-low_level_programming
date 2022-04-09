974e8d085d03ae44e951528a12436d80c51f08aa
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-04-09 16:04:40 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,25 +5,38 @@
 */
 int main(void)
 {
-	unsigned long f, t1 = 0, t2 = 1, i = 2, limit = 98, split1, split2;
+	unsigned long int i, j, k, j1, j2, k1, k2;
+	
+	j = 1;
+	k = 2;
+	
+	printf("%lu", j);
 
-	while (i <= limit)
+	for (i = 1; i < 91; i++)
 	{
-		f = t1 + t2;
-		t1 = t2;
+		printf(", %lu", k);
+		k = k + j;
 		t2 = f;
-		if (i > 91)
-		{
-			split1 = f / 100000;
-			split2 = f % 100000;
-			printf("%ld%ld\n", split1, split2);
-		}
-		else
-		{
-			printf("%ld-%ld", i, f);
-			printf(" ,\n");
-		}
-		i++;
+		j = k - j;
 	}
-	return (0);
+	
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+	
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
+	
+	printf("\n");
+	
+	return (0);	
 }
+		
