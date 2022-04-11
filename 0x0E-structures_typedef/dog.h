04c2ea36468fbae52f86f77953594be2b4e0b1c9
Shamso Osman <osmanshamso004@gmail.com>
2022-04-11 06:09:29 -0700

    dog header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,27 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H
+#define _DOG_H
 /**
-* struct dog - is a structure representation of Dog
+* struct dog - information about dogs
 * @name: name of dog
 * @age: age of dog
 * @owner: owner of dog
-* Description: is a structure representation of Dog
 */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
-void init_dog(struct dog *, char *, float, char *);
-void print_dog(struct dog *);
+/**
+* dog_t - Typedef for struct dog
+*/
+typedef struct dog dog_t;
+
+int _putchar(char);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
