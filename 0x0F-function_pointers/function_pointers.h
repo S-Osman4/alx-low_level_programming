b62731a858ebee335bd1b3ba229e0ee0d9674f0a
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-04-13 19:20:07 +0300

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,6 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
@@ -7,3 +10,5 @@ void array_iterator(int *, size_t, void (*)(int));
 int int_index(int *, int, int (*)(int));
 int valid_operator(char *);
 int to_int(char *);
+
+#endif
