cca69caae5b4aada333d19323a0c8186f01eb654
Shamso Osman <osmanshamso004@gmail.com>
2022-04-13 07:04:55 -0700

    print a name

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+void print_name(char *, void (*)(char *));
+void array_iterator(int *, size_t, void (*)(int));
+int int_index(int *, int, int (*)(int));
+int valid_operator(char *);
+int to_int(char *);
