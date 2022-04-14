7affcaeee8f477cb947c4a03764f331eec4027dc
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-04-14 23:00:27 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,7 +9,7 @@ char *symbol;
 void (*print)(va_list arg);
 } printer_t;
 
-int sum_them_all(const unsigned int n, ...);void print_numbers(const char *separator, const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_char(va_list arg);
 void print_int(va_list arg);
