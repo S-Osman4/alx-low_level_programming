b38cfe2ca82be7e3703c30929fb1bef3eb2e8bbb
Shamso Osman <osmanshamso004@gmail.com>
2022-04-26 03:58:28 -0700

    sum

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+* sum_listint - Sum the data (n) in list
+* @head: Pointer 2 the list head.
+* Return: Sum of data otherwise 0.
+*/
+int sum_listint(listint_t *head)
+{
+	int sum_listint(listint_t *head)
+	unsigned int sum = 0;
+
+	if (head)
+	{
+		while (head)
+		{
+			sum += head->n;
+			head =  head->next;
+		}
+		return (sum);
+	}
+	return (0);
+}
