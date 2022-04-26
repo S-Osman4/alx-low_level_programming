2393f2f5a3ff7660817b9e284daad3f463131545
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-04-26 13:59:34 +0300

    fix bety style

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -2,11 +2,11 @@
 /**
 * sum_listint - Sum the data (n) in list
 * @head: Pointer 2 the list head.
+* Description: This function sum of all the data (n) of a linked list
 * Return: Sum of data otherwise 0.
 */
 int sum_listint(listint_t *head)
 {
-	int sum_listint(listint_t *head)
 	unsigned int sum = 0;
 
 	if (head)
