6662043a33638d4de24fb00390a5a0fb5356d64e
Shamso Osman <osmanshamso004@gmail.com>
2022-04-26 03:38:53 -0700

    get node index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+/**
+* get_nodeint_at_index - find node by index
+* @head: Linked list
+* @index: index of the node to be finded
+* Return: listint_t node with this index
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	size_t i;
+	listint_t *tmp = NULL;
+
+	tmp = head;
+	for (i = 0 ; i < index && tmp->next; i++)
+		tmp = tmp->next;
+
+	if (i < index)
+		return (NULL);
+
+	return (tmp);
+}
