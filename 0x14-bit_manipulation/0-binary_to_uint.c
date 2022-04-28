73f00f251a1853e553d70eab14c3e337ff85d793
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-04-28 14:51:24 +0300

    fix betty style

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
 * binary_to_uint - Converts a binary number 2 unsigned int.
 * @b: String containing the binary number.
 * Return: Converted number
@@ -19,9 +19,9 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		dec_val *= 2;
+		number *= 2;
 		if (b[i] == '1')
-			dec_val += 1;
+			number += 1;
 	}
-	return (dec_val);
+	return (number);
 }
