589683d1c380470f63d6e351cfb769510439cf18
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-04-28 15:23:47 +0300

    fix bett style

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,6 +1,8 @@
 #include "main.h"
+/**
 * get_endianness - function checks the endianness.
-* Description: Endianness means that the bytes in computer memory are read in a certain order
+* Description: Endianness means that the bytes in computer memory 
+* are read in a certain order
 * Return: 0 if big endian, 1 if little endian.
 */
 int get_endianness(void)
