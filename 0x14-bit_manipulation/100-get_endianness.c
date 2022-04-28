ffd9eedad1086ee95a38757dbe67e60cf788a3c0
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-04-28 15:24:49 +0300

    fix trailing white space

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * get_endianness - function checks the endianness.
-* Description: Endianness means that the bytes in computer memory 
+* Description: Endianness means that the bytes in computer memory
 * are read in a certain order
 * Return: 0 if big endian, 1 if little endian.
 */
