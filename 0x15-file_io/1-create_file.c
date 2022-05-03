022e3dac0637685021db56bdc2b42a4d4a95a887
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-05-04 00:31:53 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,42 +1,32 @@
 #include "main.h"
+
 /**
-* create_file - Creates a file.
-* @filename: Name of the file 2 create.
-* @text_content: Text 2 write in the file.
-* Return: 1 on success, -1 on failure (file can not be created,
-* file can not be written, write “fails”, etc…)
-*/
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ * Return: If the function fails - -1.
+ * Otherwise - 1.
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, n, i;
+	int fd, w, len;
 
-	if (!filename)
-	{
+	if (filename == NULL)
 		return (-1);
-	}
 
-	if (text_content)
-	{
-		for (i = 0; text_content[i]; i++)
-		{
-			;
-		}
-	}
+	if (!text_content)
+		text_content = "";
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
-	{
-		if (fd == -1)
-		{
-			return (-1);
-		}
-	}
-	n = write(fd, text_content, i);
-	{
-		if (n == -1)
-		{
-			return (-1);
-		}
-	}
+	for (len = 0; *(text_content + len); len++)
+		;
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
 	close(fd);
+
 	return (1);
 }
