b744186e5865fc9b245f51ecbc87d9a8981b18cc
Shamso Osman <osmanshamso004@gmail.com>
2022-05-03 13:40:24 -0700

    read txt file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,3 +10,33 @@
 *. if write fails or does not write expected amunt of bytes, return 0
 */
 ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t rd, wr;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	close(fd);
+	wr = write(STDOUT_FILENO, buf, rd);
+	if (wr == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	if (wr != rd)
+		return (0);
+	return (rd);
+}
