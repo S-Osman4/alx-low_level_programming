d635978b1ae115257b48cf4603494ef0865a7b22
Shamso Osman <osmanshamso004@gmail.com>
2022-05-03 13:54:12 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+* create_file - Creates a file.
+* @filename: Name of the file 2 create.
+* @text_content: Text 2 write in the file.
+* Return: 1 on success, -1 on failure (file can not be created,
+* file can not be written, write “fails”, etc…)
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd, n, i;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+
+	if (text_content)
+	{
+		for (i = 0; text_content[i]; i++)
+		{
+			;
+		|
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	{
+		if (fd == -1)
+		{
+			return (-1);
+		}
+	}
+	n = write(fd, text_content, i);
+	{
+		if (n == -1)
+		{
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
