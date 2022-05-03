82a6f5bf23237d50eb1fd2dcec26c3582e9cf510
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-05-03 23:35:12 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,40 +7,6 @@
 *Return: the actual number of letters it could read and print_
 * .if the file can not be opened or read return 0
 * .if filename is NULL return 0
-* .if filename is NULL return 0
-* .if filename is NULL return 0
 *. if write fails or does not write expected amunt of bytes, return 0
 */
 ssize_t read_textfile(const char *filename, size_t letters)
-{
-	int file, readfile, writefile;
-	char buff;
-	unsigned int i;
-
-	if (filename == NULL)
-		return (0);
-
-	file = open(filename, O_RDWR);
-	if (file == -1)
-		return (0);
-
-	readfile = read(file, &buff, 1);
-	if (readfile == -1)
-		return (0);
-
-	for (i = 0; readfile != 0 && i != letters; i++)
-	{
-		writefile = write(STDOUT_FILENO, &buff, 1);
-		if (writefile == -1)
-			return (0);
-		readfile = read(file, &buff, 1);
-		if (readfile == -1)
-			return (0);
-	}
-
-	readfile = close(file);
-	if (readfile == -1
-		return (0);
-
-	return (i);
-}
