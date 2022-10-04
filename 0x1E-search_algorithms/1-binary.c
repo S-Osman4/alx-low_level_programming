9f53473a3bdb4ccf9a805684620ca275f7ed8ef6
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-10-04 21:37:25 +0300

    fix betty stye

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,14 +1,14 @@
 #include "search_algos.h"
-
 /**
- * binary_search - searches for a value in an array of
- * integers using the Binary search algorithm.
- * @array: Pointer to the first element of the array.
- * @size: Number of elements in array.
- * @value: Value to search for.
- * Return: The first index where value is located or -1
- * if value is not present in array or if array is NULL.
- **/
+* binary_search - searches for a value in an array of
+* integers using the Binary search algorithm.
+* @array: Pointer to the first element of the array.
+* @size: Number of elements in array.
+* @value: Value to search for.
+* Return: The first index where value is located or -1
+* if value is not present in array or if array is NULL.
+**/
+
 int binary_search(int *array, size_t size, int value)
 {
 	size_t left = 0, right = size - 1, i = 0;
